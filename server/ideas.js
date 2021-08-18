a0c671254a75b70318b8ad01a4755ec63cf49b01
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-08-18 12:36:12 +0100

    add all routes for 'ideas' url end-point

---
+++ server/ideas.js
@@ -0,0 +1,50 @@
+const ideasRouter = require('express').Router();
+
+module.exports = ideasRouter;
+
+const app = require('../server');
+const { 
+  addToDatabase,
+  getAllFromDatabase,
+  getFromDatabaseById,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require('./db');
+
+ideasRouter.param('ideaId', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+})
+
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.post('/', (req, res, next) => {
+  const addedIdea = addToDatabase('ideas', req.body);
+  res.status(201).send(addedIdea);
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.send(req.idea);
+});
+
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  let updatedIdea = updateInstanceInDatabase('ideas', req.body);
+  res.send(updatedIdea);
+});
+
+ideasRouter.delete('/ideaId', (req, res, next) => {
+  let deletedIdea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+  if (deletedIdea) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+})
